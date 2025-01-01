abf846f2b14d5f75e4210df486d858294273d8f9
larinpaul <www.larinpavel@mail.ru>
2025-01-01 10:01:41 +0300

    first commit

---
+++ 00_introduction.sql
@@ -0,0 +1,28 @@
+-- 2025 01 01
+
+-- https://careerfoundry.com/en/blog/data-analytics/advanced-sql/
+
+-- Advanced SQL For Data Analytics (Step-by-Step Tutorial)
+-- by Eric Kleppen, updated on December 1, 2022
+
+-- At his job, he is constantly digging into data
+-- with advanced SQL techniques like
+-- aggregate functions, complex table joins, and common table expressions
+
+-- In this tutorial, we'll cover the following concepts
+-- and lok at examples that will prepare you
+-- for writing advanced SQL queries for data analytics:
+-- 1. Creating data for advanced SQL example
+-- 2. Creating tables using normalization concepts
+-- 3. Joining tables
+-- 4. Writing advanced SQL joins
+-- 5. Aliasing tables and columns
+-- 6. Counting records
+-- 7. Aggregating averages in advanced SQL
+-- 8. Summary
+
+
+
+
+
+
