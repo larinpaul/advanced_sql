a2a74d496b0d6f3acac2eef3267b589f016747fd
larinpaul <www.larinpavel@mail.ru>
2025-01-02 09:56:01 +0300

    -- We can write a subquery in a JOIN clause like this: SELECT t1.column_a, t2.column_b FROM table_1 t1 INNER JOIN (SELECT column_a, column_b from table_2) t2 ON t2.column_a = t1.column_a;
    -- There are also nested joins, which are joins within joins.
    SELECT  t1.column_a, t2.column_b, t3.column_c
    FROM table_1 t1
    INNER JOIN (table_2 t2 INNER JOIN table_3 t3 on t3.column_c = t2.column_c) z on z.t2_column_a = t1.column_a

--- 04_writing_advanced_sql_joins.sql
+++ 04_writing_advanced_sql_joins.sql
@@ -10,7 +10,17 @@ SELECT t1.column_a, t2.column_b
 FORM table_1 t1
 INNER JOIN (SELECT column_a, column_b from table_2) t2 ON t2.column_a = t1.column_a;
 
+-- Joining on a subquery can be very handy in situations
+-- where it's less resource intensive to calculate statistics on the
+-- smaller dataset than trying to do it on the larger dataset.
 
+-- There are also nested joins, which are joins within joins. For example:
+SELECT t1.column_a, t2.column_b, t3.column_c
+FROM table_1 t1
+INNER JOIN (table_2 t2 
+    INNER JOIN table_3 t3 on t3.column_c = t2.column_c) z on z.t2_column_a = t1.column_a
 
-
-
+-- Nested joins are useful for when you need fields joined to one table,
+-- without impacting the joins on other tables.
+-- In the example above, we see table_2 has a nested join with table_3,
+-- and then that joined data is joined to table_1.
