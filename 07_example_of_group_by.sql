882a2cf66b68bc9f4a8d308f615cdbe9f3fc81e7
larinpaul <www.larinpavel@mail.ru>
2025-01-03 08:42:28 +0300

    Basic script format:
    SELECT column_name(s)
    
    FROM table_name
    
    WHERE condition
    
    GROUP BY column_name(s)
    
    ORDER BY column_name(s)

---
+++ 07_example_of_group_by.sql
@@ -0,0 +1,50 @@
+-- 2024 01 03 
+
+-- https://careerfoundry.com/en/blog/data-analytics/what-is-sql/
+
+-- What is SQl? The Complete Guide
+-- BY ELIOT GUNN, UPDATED ON OCTOBER 4, 2023
+
+-- Although it was created in the 1970s,
+-- SQL remains one of the most popular languages today,
+-- given its proximity to how massive datasets are managed,
+-- stored, and then retrieved.
+-- Learning SQL is also an excellent entry point
+-- into more advanced programming in data science
+-- and machine learning, too.
+
+-- 1. What is SQL?
+-- 2. What is SQL used for?
+-- 3. What are the benefits of using SQL?
+-- 4. How can I learn SQL?
+-- 5. What are the best courses for learning SQL?
+-- 6. Key takeaways
+
+-- ...
+
+-- Enter SQL (pronounced "sequel"),
+-- which stands for structured query language,
+-- a language created in 1974 by IBM 
+-- to query data in relational database management system
+-- (RDBMS).
+
+-- ...
+
+-- A simple script:
+
+SELECT column_name(s)
+
+FROM table_name
+
+WHERE condition
+
+GROUP BY column_name(s)
+
+ORDER BY column_name(s)
+
+
+
+
+
+
+
