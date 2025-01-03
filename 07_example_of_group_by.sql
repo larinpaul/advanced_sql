3ffae8e67a7637b0e186b633ca9b00a631a6df25
larinpaul <www.larinpavel@mail.ru>
2025-01-03 09:00:53 +0300

    A more complex query:
    SELECT
        column_name(s),
        duration_seconds,
        SUM(duration_seconds) OVER
            (PARTITION BY column_name(s) ORDER BY start_time)
            AS running_total,
        COUNT(duration_seconds) OVER
            (PARTITION BY column_name(s) ORDER BY start_time)
            AS running_count,
        AVG(duration_seconds) OVER
            (PARTITION BY column_name(s) ORDER BY start_time)
            AS running_avg
        FROM table
    WHERE start_time < 'date'

--- 07_example_of_group_by.sql
+++ 07_example_of_group_by.sql
@@ -42,9 +42,58 @@ GROUP BY column_name(s)
 
 ORDER BY column_name(s)
 
+-- But it can quickly incorporate much more complex logic
+-- through window functions.
+-- This snippet depicts how you can integrate 
+-- aggregation functions within a database query:
 
+SELECT  column_name(s),
+        duration_seconds,
+        SUM(duration_seconds) OVER
+            (PARTITION BY column_name(s) ORDER BY start_time) -- PARTITION BY is a clause used in conjunction with window functions to divide a result set into partitions or groups based on one or more columns -- When you use PARTITION BY in a SQL query, you're telling the database to: 1. Divide the result set int ogroups based on the columns specified in the PARTITION BY clause. 2. Apply the window function (e.g., SUM, AVG, ROW_NUMBER, etc.) to each partition separately
+            AS running_total,
+        COUNT(duration_seconds) OVER
+            (PARTITION BY column_name(s) ORDER BY start_time)
+            AS running_count,
+        AVG(duration_seconds) OVER
+            (PARTITION BY column_name(s) ORDER BY start_time)
+            AS running_avg
+    FROM table
+WHERE start_time < 'date'
 
+-- Some queries can run into more than a thousand lines of code!
+-- These examples demonstrate why SQL is so essentail to the data analytics profession.
+-- If you are already familiar with performing data transformations and aggregations
+-- from knowledge of DataFrames in Python or in R,
+-- the logic behind SQL will come easily to you
 
+-- ...
 
+-- It doesn't matter if your company uses Python or R,
+-- PowerBI or Tableau.
+-- The odds are high that they use SQL and are hiring 
+-- for basic knowledge in that.
+-- Hence, learning SQL tremendously 
+-- increases your competitiveness in the market.
+-- Even if SQL doesn't feature highly in the job description,
+-- many companies often choose to screen candidates
+-- with a SQL coding challenge
+-- as it is a useful way to test for coding ability.
 
+-- ...
 
+-- 4. How can I learn SQL?
+
+-- As a beginner, the quickest way to start learning
+-- is through online programming questions banks.
+-- e.g. LeetCode, Codewars, HackerRank.
+
+-- If you are a complete beginner to programming
+-- and would like a gentler approach to learning the basics of SQL,
+-- check our our guide to SQL to learn some beginner queries. 
+-- https://careerfoundry.com/en/blog/data-analytics/how-to-use-sql/
+-- You could also check out some popular tutorials
+-- such as SQL Zoo -- https://sqlzoo.net/wiki/SQL_Tutorial
+-- and W3 Schools -- https://www.w3schools.com/sql/default.asp
+
+-- ...
