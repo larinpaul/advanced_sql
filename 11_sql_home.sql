6c292341edddbfbb2a88ff0b2fb1afae5c020d1c
larinpaul <www.larinpavel@mail.ru>
2025-01-04 08:01:10 +0300

    -- With their online editor, you can edit the SQL statements, -- and click on a button to view the result
    SELECT * FROM Customers;

---
+++ 11_sql_home.sql
@@ -0,0 +1,8 @@
+-- 2024 01 04
+
+-- https://www.w3schools.com/sql/default.asp
+
+-- With their online editor, you can edit the SQL statements,
+-- and click on a button to view the result
+
+SELECT * FROM Customers;
