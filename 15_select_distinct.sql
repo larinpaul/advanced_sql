55e3da14e01b9b6240cee901b02fa151c0d67983
larinpaul <www.larinpavel@mail.ru>
2025-01-04 08:14:11 +0300

    SELECT DISTINCT Country FROM Customers;
    -- Syntax
    SELECT DISTINCT column1, column2, ...
    FROM table_name;

---
+++ 15_select_distinct.sql
@@ -0,0 +1,11 @@
+-- 2024 01 04
+
+-- https://www.w3schools.com/sql/sql_distinct.asp
+
+-- SQL SELECT DISTINCT Statement
+
+SELECT DISTINCT Country FROM Customers;
+
+-- Syntax
+SELECT DISTINCT column1, column2, ...
+FROM table_name;
