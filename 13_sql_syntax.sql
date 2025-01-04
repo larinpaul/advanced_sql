4e01aeb4bf97502a5a0f4ea59e92b6a968726d76
larinpaul <www.larinpavel@mail.ru>
2025-01-04 08:10:08 +0300

    SELECT UPDATE DELETE INSERT INTO CREATE DATABASE ALTER DATABASE CREATE TABLE ALTER TABLE DROP TABLE CREATE TABLE DROP INDEX

---
+++ 13_sql_syntax.sql
@@ -0,0 +1,18 @@
+-- https://www.w3schools.com/sql/sql_syntax.asp
+
+-- 2024 01 04
+
+-- Some of the most important SQL Commands
+SELECT -- extract data from a database
+UPDATE -- updates data in a database
+DELETE -- deletes data from a database
+INSERT INTO -- inserts new data into a database
+CREATE DATABASE -- creates a new database
+ALTER DATABASE -- mdofies a database
+CREATE TABLE -- creates a table
+ALTER TABLE -- modifies a table
+DROP TABLE -- deletes a key
+CREATE TABLE -- creates an index (search key)
+DROP INDEX  -- deletes an index
+
+
