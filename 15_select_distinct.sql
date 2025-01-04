1afc5a707845f1d785738b20c4e5fd96fd5e06eb
larinpaul <www.larinpavel@mail.ru>
2025-01-04 08:16:58 +0300

    SELECT Count(*) AS DistinctCountries FROM (SELECT DISTINCT Country FROM Customers);

--- 15_select_distinct.sql
+++ 15_select_distinct.sql
@@ -12,3 +12,10 @@ FROM table_name;
 
 -- Count distinct
 SELECT COUNT(DISTINCT Country) FROM Customers;
+
+-- Some RDBMS don't support SELECT COUNT(DISTINCT Country)
+-- for example MS Access
+-- so you have to use something like this
+
+SELECT Count(*) AS DistinctCountries
+FROM (SELECT DISTINCT Country FROM Customers);
