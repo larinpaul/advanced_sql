bac36afe44cd6b5732c31a4b9610d54100b84aaa
larinpaul <www.larinpavel@mail.ru>
2025-01-05 15:02:48 +0300

    -- MySQL String Functions ASCII CHAR_LENGTH CHARACTER_LENGTH CONCAT CONCAT_WS FIELD FIND_IN_SET FORMAT INSERT INSTR LCASE LEFT LENGTH LOCATE LOWER LPAN LTRIM MID POSTION REPEAT REPLACE REVERSE RIGHT RPAD RTRIM SPACE STRCMP SUBSTR SUBSTRING SUBSTRING_INDEX TRIM UCASE UPPER
    -- MySQL Numeric Functions
    ABS
    ACOS
    ASIN
    ATAN
    ATAN2
    AVG
    CEIL
    CEILING
    COS
    COT
    COUNT
    DEGREES
    DIV
    EXP
    FLOOR
    GREATEST
    LEAST
    LN
    LOG
    LOG10
    LOG2
    MAX
    MIN
    MOD
    PI
    POW
    POWER
    RADIANS
    RAND
    ROUND
    SIGN
    SIN
    SQRT
    SUM
    TAN
    TRUNCATE
    
    -- MySQL Date Functions
    ADDDATE
    ADDTIME
    CURDATE
    CURRENT_DATE
    CURRENT_TIME
    CURRENT_TIMESTAMP
    CURTIME
    DATE
    DATEDIFF
    DATE_ADD
    DATE_FORMAT
    DATE_SUB
    DAY
    DAYNAME
    DAYOFMONTH
    DAYOFWEEK
    DAYOFYEAR
    EXTRACT
    FROM_DAYS
    HOUR
    LAST_DAY
    LOCALTIME
    LOCALTIMESTAMP
    MAKEDATE
    MAKETIME
    MICROSECOND
    MINUTE
    MONTH
    MONTHNAME
    NOW
    PERIOD_ADD
    PERIOD_DIFF
    QUARTER
    SECOND
    SEC_TO_TIME
    STR_TO_DATE
    SUBDATE
    SUBTIME
    SYSDATE
    TIME
    TIME_FORMAT
    TIME_TO_SEC
    TIMEDIFF
    TIMESTAMP
    TO_DAYS
    WEEK
    WEEKDAY
    WEEKOFYEAR
    YEAR
    YEARWEEK
    
    -- MySQL Advanced Functions
    BIN
    BINARY
    CASE
    CAST
    COALESCE
    CONNECTION_ID
    CONV
    CONVERT
    CURRENT_USER
    DATABASE
    IF
    IFNULL
    ISNULL
    LAST_INSERT_ID
    NULLIF
    SESSION_USER
    SYSTEM_USER
    USER
    VERSION

--- 112_mysql_functions.sql
+++ 112_mysql_functions.sql
@@ -132,21 +132,22 @@ YEAR -- Returns the year part for a given date
 YEARWEEK -- Returns the year and week number for a given date
 
 -- MySQL Advanced Functions
-BIN
-BINARY
-CASE
-CAST
-COALESCE
-CONNECTION_ID
-CONV
-CONVERT
-CURRENT_USER
-DATABASE
-IF
-ISNULL
-LAST_INSERT_ID
-NULLIF
-SESSION_USER
-SYSTEM_USER
-USER
-VERSION
+BIN -- Returns a binary representation of a number
+BINARY -- Converts a value to a binary string
+CASE -- Goes through conditions and returns a value when the first condition is met
+CAST -- Converts a value (of any type) into a specified datatype
+COALESCE -- Returns the first non-null value in a list
+CONNECTION_ID -- Returns the unique connection ID for the current connection
+CONV -- Converts a number from one numeric base system to another
+CONVERT -- Converts a value into the specified datatype or character set
+CURRENT_USER -- Returns the user name and host name for the MySQL account that the server used to authenticate the current client
+DATABASE -- Returns the name of the current database
+IF -- Returns a value if a condition is TRUE, or another value if a condition is FALSE
+IFNULL -- Returns a specified value if the expression is NULL, otherwise return the expression
+ISNULL -- Returns 1 or 0 depending on whether an expression is NULL
+LAST_INSERT_ID -- Returns the AUTO_INCREMENT id of the last row that has been inserted in a table
+NULLIF -- Compares two expressions and returns NULL if they are equal. Otherwise, the first expression is returned
+SESSION_USER -- Returns the current MySQL user name and host name
+SYSTEM_USER -- Returns the current MySQL user name and host name
+USER -- Returns the current MySQL user name and host name
+VERSION -- Returns the current version of the MySQL database
