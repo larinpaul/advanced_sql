db57dd939e940a7920b3540bba1a882c7c36ba33
larinpaul <www.larinpavel@mail.ru>
2025-01-06 07:16:47 +0300

    Started writing SQL Quick Reference

---
+++ 113_sql_quick_reference.sql
@@ -0,0 +1,23 @@
+-- 2025 01 06
+
+-- https://www.w3schools.com/sql/sql_quickref.asp
+
+-- SQL Quick Reference from W3School
+
+
+-- SQL Statement
+-- Syntax
+
+-- AND / OR
+
+SELECT column_name(s)
+FROM table_name
+WHERE condition1
+AND condition2;
+
+SELECT column_name(s)
+FROM table_name
+WHERE  condition1
+OR condition2;
+
+
