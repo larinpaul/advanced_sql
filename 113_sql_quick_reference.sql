1fdc1442de29eb38996154cf17886158ce084e8d
larinpaul <www.larinpavel@mail.ru>
2025-01-08 05:38:58 +0300

    -- HAVING SELECT column_name, aggregate_function(column_name) FROM table_name WHERE column_name operator value GROUP BY column_name HAVING aggregate_function(column_name) operator value

--- 113_sql_quick_reference.sql
+++ 113_sql_quick_reference.sql
@@ -133,4 +133,13 @@ WHERE column_name operator value
 GROUP BY column_name
 
 
+-- HAVING
+SELECT column_name, aggregate_function(column_name)
+FROM table_name
+WHERE column_name operator value
+GROUP BY column_name
+HAVING aggregate_function(column_name) operator value
+
+
+-- IN
 
