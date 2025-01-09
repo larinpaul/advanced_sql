62141e93283902fe155dc8efd6e39067afbe2f2e
larinpaul <www.larinpavel@mail.ru>
2025-01-09 05:30:02 +0300

    An aggregate function is a function that performs a calculation on a set of values, and returns a single value.
    Aggregate functions are often used with the GROUP BY clause of the SELECT statement.
    The GROUP BY clause splits the result-set into groups of values and the aggregate function can be used to return a single value for each group.
    
    The most commonly used SQL aggregate functions are:
    MIN()
    MAX()
    COUNT()
    SUM()
    AVG()
    
    Aggregate functions ignore null values (except for COUNT() )

---
+++ 16_sql_aggregate_functions.sql
@@ -0,0 +1,21 @@
+-- 2024 01 09
+
+-- SQL Aggregate Functions
+
+-- https://www.w3schools.com/sql/sql_aggregate_functions.asp
+
+
+-- An aggregate function is a function that performs a calculation on a set of values, and returns a single value.
+
+-- Aggregate functions are often used with the GROUP BY clause of the SELECT statement.
+-- The GROUP BY clause spits the result-set into groups of values
+-- and the aggregate function can be used to return a single value for each group.
+
+-- The most commonly used SQL aggregate functions are:
+-- * MIN() - returns the smallest value within the selected column
+-- * MAX() - returns the largest value within the selected column
+-- * COUNT() - returns teh number of rows in  a set
+-- * SUM() - returns the total sum of a numerical column
+-- * AVG() - returns the average value of a numerical column
+
+-- Aggregate functions ignore null values (except for COUNT() ).
