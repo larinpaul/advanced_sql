0085fcb07742f7f853d6164f22d0cf0ff5d74b01
larinpaul <www.larinpavel@mail.ru>
2025-01-15 07:07:28 +0300

    -- SQL Wildcard Characters
    -- %
    _
    []
    -
    Combine
    Without

---
+++ 24_sql_wildcards.sql
@@ -0,0 +1,36 @@
+-- 2024 01 15
+
+-- SQL Wildcard Characters
+
+-- A wildcard character is used to substitute one or more characters in a string.
+
+-- Wildcard characters are used with the LIKE operator.
+-- The LIKE operator is used in a WHERE clause to search for a specified pattern in a column.
+
+
+-- Wildcard Characters
+
+
+-- Demo Database
+
+
+-- Using the % Wildcard
+
+
+-- Using the _ Wildcard
+
+
+-- Using the [] Wildcard
+
+
+-- Using the - Wildcard
+
+
+-- Combine Wildcards
+
+
+-- Without Wildcard
+
+
+-- Microsoft Access Wildcards
+
