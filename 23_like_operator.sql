d559f4ebdd58b37ab2e45635b1e0904b7ea2120a
larinpaul <www.larinpavel@mail.ru>
2025-01-15 06:41:49 +0300

    -- The _ wildcard
    SELECT * FROM Customers
    WHERE city LIKE 'L_nd__';
    
    -- The % wildcard
    SELECT * FROM Customers
    WHERE city LIKE '%L%';
    
    -- Starts with
    SELECT * FROM Customers
    WHERE CustomerName LIKE 'La%';
    
    -- Tip: You can also combine any number of conditions using AND or OR operators
    
    SELECT * FROM Customers
    WHERE CustomerName LIKE 'a%' OR CustomerName LIKE 'b%';

--- 23_like_operator.sql
+++ 23_like_operator.sql
@@ -23,3 +23,46 @@ FROM table_name
 WHERE columnN LIKE pattern;
 
 
+-- Demo Database
+
+-- The Customers table:
+CustomerID	CustomerName	ContactName	Address	City	PostalCode	Country
+1   Alfreds Futterkiste	Maria Anders	Obere Str. 57	Berlin	12209	Germany
+2	Ana Trujillo Emparedados y helados	Ana Trujillo	Avda. de la Constitución 2222	México D.F.	05021	Mexico
+3	Antonio Moreno Taquería	Antonio Moreno	Mataderos 2312	México D.F.	05023	Mexico
+4   Around the Horn	Thomas Hardy	120 Hanover Sq.	London	WA1 1DP	UK
+5	Berglunds snabbköp	Christina Berglund	Berguvsvägen 8	Luleå	S-958 22	Swede
+
+
+-- The _ Wildcard
+
+-- The _ wildcard represents a single character
+
+-- It can be any character or number, but each _ represents one, and ony one, character.
+
+-- Return all customers form a city that starts with 'L' followed by one wildcard character,
+-- then 'nd' and then two wildcard characters:
+SELECT * FROM Customers
+WHERE city LIKE 'L_nd__';
+
+
+-- The % Wildcard
+
+-- The % wildcard represents any number of characters, even zero characters.
+
+-- Return all customers from a city that CONTAINS the letter 'L':
+SELECT * FROM Customers
+WHERE city LIKE '%L%';
+
+-- Starts With
+
+-- To return records that start with a specific letter of phrase, add % at the end of the letter or phrase.
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'La%';
+
+-- Tip: You can also combine any number of conditions using AND or OR operators.
+
+-- Return all customers that start with 'a' or start with 'b':
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'a%' OR CustomerName LIKE 'b%';
+
