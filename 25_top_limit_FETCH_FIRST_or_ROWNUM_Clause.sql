9be6b69fd568c17a2ab8e53a9d703eaeda912992
larinpaul <www.larinpavel@mail.ru>
2025-01-16 06:56:57 +0300

    Started learning TOP, LIMIT, FETCH FIRST or ROWNUM Clause

---
+++ 25_top_limit_FETCH_FIRST_or_ROWNUM_Clause.sql
@@ -0,0 +1,32 @@
+-- 2025 01 16
+
+-- SQL TOP, LIMIT, FETCH FIRST or ROWNUM Clause
+
+
+-- The SQL SELECT TOP Clause
+
+
+-- Demo Database
+
+
+-- LIMIT
+
+
+-- FETCH FIRST
+
+
+-- SQL TOP PERCENT Example
+
+
+-- ADD a WHERE CLAUSE
+
+
+-- ADD the ORDER BY Keyword
+
+
+
+
+
+
+
+
