57ef70ad06c6f64da79c1199c8d601f6dae1f4ca
larinpaul <www.larinpavel@mail.ru>
2025-01-16 06:53:10 +0300

    -- Microsoft Access Wildcards * -- bl* finds bl, black, blue, and blob ? -- h?t finds hot, hat, and hit [] -- h[oa]t finds hot and hat, but not hit ! -- h[!oa]t finds hit, but not hot and hat - -- c[a-b]t finds cat and cbt # -- any single numeric character: 2#5 finds 205, 215, 295...

--- 24_sql_wildcards.sql
+++ 24_sql_wildcards.sql
@@ -88,3 +88,10 @@ WHERE Country LIKE 'Spain'
 -- The Microsoft Access Database has some other wildcards:
 
 -- Symbol -- Description -- Example
+
+* -- Represents zero or more characters -- bl* filds bl, black, blue, and blob
+? -- Represents a single character -- h?t finds hot, hat, and hit
+[] -- Represents any single character within the brackets -- h[oa]t finds hot and hat, but not hit
+! -- Represents any character not in the brackets -- h[!oa]t finds hit, but not hot and hat
+- -- Represents any single character within the specified range - c[a-b]t finds cat and cbt
+# -- Represents any single numeric character -- 2#5 finds 205, 215, 225, 235, 245, 255, 265, 275, 285, and 295
