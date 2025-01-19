e0e801fd77506694a5f8dfc2d7be62dc4f8cf8e0
larinpaul <www.larinpavel@mail.ru>
2025-01-19 17:12:12 +0300

    -- SQL Server
    -- The SQL Server ISNULL() function lets you return an alternative value when an expression is NULL:
    
    SELECT ProductName, UnitPrice * (UnitsInStock + ISNULL(UnitsOnOrder, 0))
    FROM Products;
    
    -- or we can use the COALESCE() function, like this:
    
    SELECT ProductName, UnitPrice * (UnitsInStock + COALESCE(UnitsOnOrder, 0))
    FROM Products;
    
    -- MS Access
    
    -- The MS Access IsNull() function returns TRUE (-1) if the expression is a null value, otherwise FALSE (0):
    SELECT ProductName, UnitPrice * (UnitsInStock = IIF(IsNull(UnitsOnOrder), 0, UnitsOnOrder))
    FROM Products;
    
    -- Oracle
    
    -- The Oracle NVL() function achieves the same result:
    
    SELECT ProductName, UnitPrice * (UnitsInStock + NVL(UnitsOnOrder, 0))
    FROM Products;
    
    -- or we can use the COALESCE() function, like this:
    
    SELECT ProductName, UnitPrice * (UnitsInStock + COALESCE(UnitsOnOrder, 0))
    FROM Products;
    
    -- Solution in PostgreSQL
    
    -- Using COALESCE()
    
    -- The COALESCE() function is the standard way to handle NULL values in PostgreSQL.
    -- It returns the first non-NULL value in the list of arguments.
    
    SELECT
       ProductName,
       UnitPrice * (UnitsInStock + COALESCE(UnitsOnOrder, 0)) AS TotalValue
    FROM
        Products;

--- 27_sql_null_functions.sql
+++ 27_sql_null_functions.sql
@@ -52,16 +52,45 @@ FROM YourTable;
 
 -- SQL Server
 
+-- The SQL Server ISNULL() function lets you return an alternative value when an expression is NULL:
+
+SELECT ProductName, UnitPrice * (UnitsInStock + ISNULL(UnitsOnOrder, 0))
+FROM Products;
+
+-- or we can use the COALESCE() function, like this:
+
+SELECT ProductName, UnitPrice * (UnitsInStock + COALESCE(UnitsOnOrder, 0))
+FROM Products;
+
 
 -- MS Access
+
+-- The MS Access IsNull() function returns TRUE (-1) if the expression is a null value, otherwise FALSE (0):
+SELECT ProductName, UnitPrice * (UnitsInStock + IIF(IsNull(UnitsOnOrder), 0, UnitsOnOrder))
+FROM Products;
 
 
 -- Oracle
 
+-- The Oracle NVL() function achieves the same result:
 
--- 
+SELECT ProductName, UnitPrice * (UnitsInStock + NVL(UnitsOnOrder, 0))
+FROM Products;
 
+-- or we can use the COALESCE() function, like this:
 
+SELECT ProductName, UnitPrice * (UnitsInStock + COALESCE(UnitsOnOrder, 0))
+FROM Products;
 
+-- Solution in PostgreSQL
 
+-- Using COALESCE()
 
+-- The COALESCE() function is the standard way to handle NULL values in PostgreSQL.
+-- It returns the first non-NULL value in the list of arguments.
+
+SELECT
+    ProductName,
+    UnitPrice * (UnitsInStock + COALESCE(UnitsOnOrder, 0)) AS TotalValue
+FROM
+    Products;
