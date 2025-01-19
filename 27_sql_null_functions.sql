67aec2c32bc3dea2d7dd2c9b23401459fedddc4c
larinpaul <www.larinpavel@mail.ru>
2025-01-19 16:33:40 +0300

    -- MySQL
    -- The MySQL IFNULL() function lets you return an alternative if an expression is NULL:
    SELECT ProductName, UnitPrice * (UnitsInStock + IFNULL(UnitsOnOrder, 0))
    
    -- or we can use the COALESCE() function, like this:
    
    SELECT ProductName, UnitPrice * (UnitsInStock + COALESCE(UnitsOnOrder, 0))
    FROM Products;

--- 27_sql_null_functions.sql
+++ 27_sql_null_functions.sql
@@ -28,6 +28,15 @@ FROM Products;
 
 -- MySQL
 
+-- The MySQL IFNULL() function lets you return an alternative if an expression is NULL:
+SELECT ProductName, UnitPrice * (UnitsInStock + IFNULL(UnitsOnOrder, 0))
+FROM Products;
+
+-- or we can use the COALESCE() function, like this:
+
+SELECT ProductName, UnitPrice * (UnitsInStock + COALESCE(UnitsOnOrder, 0))
+FROM Products;
+
 
 -- SQL Server
 
