77d3e8e5cd9ff91ff50bc9a31d7885be91bcd184
larinpaul <www.larinpavel@mail.ru>
2025-01-20 06:30:53 +0300

    -- A stored procedure is a prepared SQL code that you can save, so thecode can be reused over and over again.
    -- Stored Procedure Syntax
    CREATE PROCEDURE procedure_name
    AS
    sql_statement
    GO;
    
    -- Execute a Stored Procedure
    
    EXEC procedure_name;

---
+++ 28_stored_procedures_for_sql_server.sql
@@ -0,0 +1,42 @@
+-- 2025 01 20
+
+
+-- What is a Stored Procedure?
+
+-- A stored procedure is a prepared SQL code that you can save,
+-- so the code can be reused over and over again.
+
+-- So if you have an SQL query that you write over and over again,
+-- save it as a astored procedure, and then just call it to execute it.
+
+-- You can also pass arguments to a stored procedure,
+-- so that the stored procedure can act based on the parameter value(s) that is passed.
+
+
+-- Stored Procedure Syntax
+
+CREATE PROCEDURE procedure_name
+AS
+sql_statement
+GO;
+
+-- Execute a Stored Procedure
+
+EXEC procedure_name;
+
+
+-- Demo Database
+
+
+-- Stored Procedure Example
+
+
+-- Stored Procedure With One Parameter
+
+
+-- Stored Procedure With Multiple Parameters
+
+
+
+
+
