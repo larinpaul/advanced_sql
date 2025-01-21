383bf74f46915c260f14b6914a5008e3dbde6ff3
larinpaul <www.larinpavel@mail.ru>
2025-01-21 06:53:11 +0300

    -- SQL Operators
    -- SQL Arithmetic Operators
    +
    -
    *
    /
    %
    
    -- SQL Bitwise Operators
    &
    |
    ^
    
    -- SQL Comparison Operators
    =
    >
    <
    >=
    <=
    <>
    
    -- SQL Compound Operators
    +=
    -=
    *=
    /=
    %=
    &=
    ^-=
    |*=
    
    -- SQL Logical Operators
    
    ALL
    AND
    ANY
    BETWEEN
    EXISTS
    IN
    LIKE
    NOT
    OR
    SOME

---
+++ 29_sql_operators.sql
@@ -0,0 +1,55 @@
+-- 2025 01 21 
+
+-- SQL Operator
+-- https://www.w3schools.com/sql/sql_operators.asp
+
+-- SQL Arithmetic Operators
+
+-- Operator -- Description
++ -- Add
+- -- Subtract
+* -- Multiply
+/ -- Divide
+% -- Modulo
+
+-- SQL Bitwise Operators
+
+-- Operator -- Description
+& -- Bitwise AND
+| -- Bitwise OR
+^ -- Bitwise exclusive OR
+
+-- SQL Comparison Operators
+
+-- Operator Description
+= -- Equal to
+> -- Greater than
+< -- Less than
+>= -- Greater than or equal to
+<= -- Less than or equal to
+<> -- Not equal to
+
+-- SQL Compound Operators
+
+-- Operator -- Description
++= -- Add equals
+-= -- Subtract equals
+*= -- Multiply equals
+/= -- Divide equals
+%= -- Modulo equals
+&= -- Bitwise AND equals
+^-= -- Bitwise exclusive equals
+|*= -- Bitwise OR equals
+
+-- SQL Logical Operators
+
+ALL -- TRUE if all of the subquery values meet the condition
+AND -- TRUE if all the conditions separated by AND is TRUE
+ANY -- TRUE if any of the subquery values meet the condition
+BETWEEN -- TRUE if the operand is within the range of comparisons
+EXISTS -- TRUE if the subquery returns one or more records
+IN -- TRUE if the operand is equal to one of a list of expressions
+LIKE -- TRUE if the operand matches a pattern
+NOT -- Display a record if the condition(s) is NOT TURE
+OR -- TRUE if any of the conditions separated by OR is TRUE
+SOME -- TRUE if any of the subquery values meet the condition
