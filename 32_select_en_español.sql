c694ab5799135e6fc25c301498506531de17d22e
larinpaul <www.larinpavel@mail.ru>
2025-01-24 07:28:12 +0300

    -- Auf Deutsch
    -- Contents
    1 Einführung in die Arbeit mit der Tabelle der BBC-Länderprofile
    2 Pro-Kopf-Einkommen
    3 Klein und reich
    4 Skandinavien
    5 Fängt mit G an
    6 Genau die richtige Größe
    
    -- Einführung in die Arbeit mit der Tabelle der BBC-Länderprofile. Dieses Tutorium stellst SQL als Abfragesprache vor. Die SELECT-Anweisung wird am Beispiel der Tabelle bbc demonstriert. Zusammenfassung
    
    -- 1. Das baispiel zeigt die Bevölkerungszahl von Frankreich ('France'). Zeichketten (String) sind mit 'einfachem Hochkomma' anzugeben. Zeige die Bevölkerungszalh von Deutschland ('Germany').
    SELECT population FROM bbc WHERE name = 'France';

--- 32_select_en_español.sql
+++ 32_select_en_español.sql
@@ -18,3 +18,31 @@ SELECT name, population FROM world
 SELECT name, area FROM world
     WHERE area BETWEEN 250000 AND 300000;
 
+
+-- Auf Deutsch
+-- https://sqlzoo.net/wiki/SELECT_basics/de
+
+-- Contents -- hide
+-- 1 Einführung in die Arbeit mit der Tabelle der BBC-Länderprofile
+-- 2 Pro-Kopf-Einkommen
+-- 3 Klein und reich
+-- 4 Skandinavien
+-- 5 Fängt mit G an
+-- 6 Genau die richtige Größe
+
+
+-- Einführung in die Arbeit mit der Tabelle der BBC-Länderprofile
+-- Dieses Tutorium stellst SQL als Abfragesprache vor.
+-- Die SELECT-Anweisung wird am Beispiel der Tabelle bbc demonstriert.
+-- Zusammenfassung
+
+
+-- 1.
+-- Bas Beispiel zeigt die Bevölkerungszahl von Frankreich ('France').
+-- Zeichketten (String) sind mit 'einfachem Hochkomma' anzugeben.
+-- Zeige die Bevölkerungszahl von Deutschland ('Germany').
+SELECT population FROM bbc
+    WHERE name = 'France';
+
+
+
