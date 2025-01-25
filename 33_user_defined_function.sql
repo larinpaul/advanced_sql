b863561b271160bedf19c33924085a966cff261f
larinpaul <www.larinpavel@mail.ru>
2025-01-25 07:42:14 +0300

    -- A Scalar function accepts one or more parameters and returns a single value.
    -- A Table-Valued function takes one input parameter and returns the columns of the table
    
    -- Requires CREATE FUNCTION permission in the database and ALTER permission on the schema in which the functions are being created.
    
    -- Syntax
    
    -- 1. T-SQL Scalar function syntax
    
    CREATE FUNCTION [ schema_name. ] <function_name> [
    (@parameterName datatype)]
    RETURNS return_data_type
    BEGIN
        function_body --T-SQL statements
        RETURN scalar_expression
    END;

--- 33_user_defined_function.sql
+++ 33_user_defined_function.sql
@@ -4,5 +4,30 @@
 -- https://hasura.io/learn/database/microsoft-sql-server/create-function/
 
 
+-- MS SQL user-defined functions are of 2 types: Scalar and Tabular-Valued
+-- based on the type of result set each return.
+
+-- A Scalar function accepts one or more parameters and returns a single value.
+
+-- A Table-Valued function takes one input parameter
+-- and returns the columns of the table
+
+-- Requires CREATE FUNCTION permission in the database
+-- and ALTER permission on the schema in which the functions are being created.
+-- [ as referred in the Microsoft Docs ]
+
+
+-- Syntax
+
+-- 1. T-SQL Scalar function syntax
+
+CREATE FUNCTION [ schema_name. ] <function_name> [
+(@parameterName datatype)]
+RETURNS return_data_type
+BEGIN
+    function_body --T-SQL statements
+    RETURN scalar_expression
+END;
+
 
 
