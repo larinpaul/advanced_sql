a9a1867255450a951410ac111d93e68e2d0ee73c
larinpaul <www.larinpavel@mail.ru>
2025-01-25 07:32:15 +0300

    -- Pro-Kopf-Einkommen -- 2. SELECT name, population/area FROM bbc     WHERE area > 5000000

--- 32_select_en_español.sql
+++ 32_select_en_español.sql
@@ -45,4 +45,57 @@ SELECT population FROM bbc
     WHERE name = 'France';
 
 
+-- Pro-Kopf-Einkommen
+-- 2.
+-- Diese Abfrage zeight die Bevölkerungsdichte (population/area)
+-- jedes Landes mit einer Fläche größer als 5.000.000 km^2.
+-- Zeige das Pro-Kopf-Einkommen (gdp/population)
+-- für jedes Land mit einer Fläche größer als 5.000.000 km^2.
+SELECT name, population/area FROM bbc
+    WHERE area > 5000000
+
+
+-- Klein und reich
+-- 3.
+-- Welche Länder sind sowoch sehr klein auch sehr reich?
+-- Wir verwenden AND um sicherzustellen, dass nur dann ein Ergebnis geliefert wird,
+-- wenn alle (Teil-)Bedingungen wahr sind.
+-- Das Beispiel zeight die Länder mit geringer Bevölkerungszahl
+-- und hohem Buttoinlandsprodukt (GRP).
+-- Zeige die Names (name) und Regionen (region) der Länder
+-- mit einer Fläche (area) kleiner 2.000 km^2
+-- und einem Bruttoninlandsprodukt größer als 5 Milliardern (= 5.000.000.000).
+SELECT name, region
+    FROM bbc
+    WHERE population < 2000000
+        AND gdp > 5000000000
+
+
+-- Skandinavien
+-- 4.
+-- Mit dem Wort IN können wir prüfen,
+-- ob ein Element Bestandteil einer (Werte-)Liste ist.
+-- Das Beispiel zeigt Ländernamen (name) und Bevölkerungszahl (population)
+-- der Länder Irland('Ireland'), Island ('Iceland') und Dänemark('Denmark').
+-- Zeige Ländernamen und Bevölkerungszahlen der folgenden Länder:
+-- 'Denmark', 'Finland', 'Norway', 'Sweden'.
+SELECT name, population FROM bbc
+    WHERE name IN ('Ireland', 'Iceland', 'Denmark')
+
+
+-- Fängt mit G an
+-- 5.
+-- Welche Ländernamen beginnen mit dem Buchstaben G?
+-- Das Wort LIKE ermöglicht einen Mustervergleich.
+-- Das Prozentzeichen (%) ist der Platzhalter (Wildcard).
+-- Das Beispiel zeight Länder, deren Namen mit dem Buchstaben D beginnen.
+-- Zeige Länder, deren Namen mit dem Buchstaben G beginnen.
+SELECTG name FROM bbc
+    WHERE name LIKE 'D%'
+
+
+-- Genau die richtige Größe
+-- 6.
+-- Welche Länder sind weder zu klein noch zu groß?
+-- BE
 
