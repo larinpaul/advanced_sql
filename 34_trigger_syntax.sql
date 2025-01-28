362039d3fbf4b95fc8de60a2f31d6abe3dd0632e
larinpaul <www.larinpavel@mail.ru>
2025-01-28 06:08:18 +0300

    -- Triggers can contain direct references to tables by name, -- such as the trigger named testref shown in this example CREATE TABLE test1(a1 INT); CREATE TABLE test2(a2 INT); CREATE TABLE test3(a3 INT NOT NULL AUTO_INCREMENT PRIMARY KEY); CREATE TABLE test4(     a4 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,     b4 INT DEFAULT 0 );
    delimiter | -- When defining triggers, stored procedures, or functions that contain multiple SQL statements, you need to change the delimiter to something else (like |) so that MySQL does not interpret the semicolons within the body of the trigger as the end of the entire statement.
    
    CREATE TRIGGER testref BEFORE INSERT ON test1
        FOR EACH ROW
        BEGIN
            INSERT INTO test2 SET a2 = NEW.a1;
            DELETE FROM test3 WHERE a3 = NEW.a1;
            UPDATE test4 SET b4 = b4 + 1 WHERE a4 = NEW.a1;
        END;
    |
    
    delimiter ;
    
    INSERT INTO test3 (a3) VALUES
        (NULL), (NULL), (NULL), (NULL), (NULL),
        (NULL), (NULL), (NULL), (NULL), (NULL);
    
    INSERT INTO test4 (a4) VALUES
        (0), (0), (0), (0), (0), (0), (0), (0), (0), (0);

--- 34_trigger_syntax.sql
+++ 34_trigger_syntax.sql
@@ -112,6 +112,36 @@ delimiter ;
 
 -- See also Section 27.8, "Restrictions on Stored Programs".
 
+-- Error handling...
 
+-- ...
 
+-- Triggers can contain direct references to tables by name,
+-- such as the trigger named testref shown in this example
+CREATE TABLE test1(a1 INT);
+CREATE TABLE test2(a2 INT);
+CREATE TABLE test3(a3 INT NOT NULL AUTO_INCREMENT PRIMARY KEY);
+CREATE TABLE test4(
+    a4 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    b4 INT DEFAULT 0
+);
 
+delimiter | -- When defining triggers, stored procedures, or functions that contain multiple SQL statements, you need to change the delimiter to something else (like |) so that MySQL does not interpret the semicolons within the body of the trigger as the end of the entire statement.
+
+CREATE TRIGGER testref BEFORE INSERT ON test1
+    FOR EACH ROW
+    BEGIN
+        INSERT INTO test2 SET a2 = NEW.a1;
+        DELETE FROM test3 WHERE a3 = NEW.a1;
+        UPDATE test4 SET b4 = b4 + 1 WHERE a4 = NEW.a1;
+    END;
+|
+
+delimiter ;
+
+INSERT INTO test3 (a3) VALUES
+    (NULL), (NULL), (NULL), (NULL), (NULL), 
+    (NULL), (NULL), (NULL), (NULL), (NULL);
+
+INSERT INTO test4 (a4) VALUES
+    (0), (0), (0), (0), (0), (0), (0), (0), (0), (0);
