60609976665f3609531853b24c695b354cd7bdfe
larinpaul <www.larinpavel@mail.ru>
2025-01-31 06:01:52 +0300

    -- 14.1 Built-In Function and Operator Reference -- https://dev.mysql.com/doc/refman/8.4/en/built-in-function-reference.html

---
+++ 36_built-in_function_and_operator_reference.sql
@@ -0,0 +1,18 @@
+-- 2025 01 31
+
+-- 14.1 Built-In Function and Operator Reference
+-- https://dev.mysql.com/doc/refman/8.4/en/built-in-function-reference.html
+
+-- The following table lists each built-in (native) function and operator
+-- and provides a short description of each one.
+-- For a table listing function that are loadable at runtime, see 
+-- Section 14.2, "Loadable Function Reference" -- https://dev.mysql.com/doc/refman/8.4/en/loadable-function-reference.html
+
+
+-- Table 14.1. Built-In Functions and Operators
+
+-- Name -- Description -- Deprecated
+
+
+
+
