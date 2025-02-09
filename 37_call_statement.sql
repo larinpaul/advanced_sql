92785840575ffd721587bc0ad707d215d2e6f6db
larinpaul <www.larinpavel@mail.ru>
2025-02-09 09:17:34 +0300

    DELIMITER //
    CREATE PROCEDURE p (OUT ver_param VARCHAR(25), INOUT incr_param INT)
    BEGIN
        # Set value of OUT parameter
        SELECT VERSION() INTO ver_param;
        # Incrtement value of INOUT parameter
        SET incr_param = incr_param + 1;
    END //
    
    DELIMITER ;

--- 37_call_statement.sql
+++ 37_call_statement.sql
@@ -6,3 +6,37 @@
 
 CALL sp_name([parameter[,...]])
 CALL sp_name([])
+
+
+-- Stored procedures that take no arguments can be invoked without parentheses.
+-- That is, CALL p() and CALL p are equivalent.
+
+-- CALL can pass back values to its caller using parameters that are declared as OUT or INOUT.
+-- A client program can also obtain the number of rows affected:
+-- At the SQL level, call the ROW_COUNT() function;
+-- from the C API, call the mysql_affected_rows() function.
+
+-- For information about the effect of unhandled conditions on procedure parameters, see 
+-- Section 15.6.7.8. "Condition Handling and OUT or INOUT Paremeters".
+-- https://dev.mysql.com/doc/refman/8.4/en/conditions-and-parameters.html
+
+-- ...
+
+-- The following procedure has an OUT parameter that the procedure sets to the current server version,
+-- and an INOUT value that the procedure increments by one from its current value:
+
+DELIMITER //
+
+CREATE PROCEDURE p (OUT ver_param VARCHAR(25), INOUT incr_param INT)
+BEGIN
+    # Set value of OUT parameter
+    SELECT VERSION() INTO ver_param;
+    # Increment value of INOUT parameter
+    SET incr_param = incr_param + 1;
+END //
+
+DELIMITER ;
+
+
+
+
