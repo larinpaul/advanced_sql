15511e4c7767fe39015b9e7c25182835920f46c7
larinpaul <www.larinpavel@mail.ru>
2025-02-11 06:11:50 +0300

    7.7. MySQL Server Loadable Functions

---
+++ 38_loadable_functions.sql
@@ -0,0 +1,21 @@
+--  2025 02 11 -- 06:07
+
+-- 7.7. MySQL Server Loadable Functions
+-- https://dev.mysql.com/doc/refman/8.4/en/server-loadable-functions.html
+
+-- MySQL Supports loadable functions, that is, functions that are not built in
+-- but can be loaded at runtime (either during startup or later) to extend server capabilities,
+-- or unloaded to remove capabilities.
+-- For a table describing the available loadable functions, see Section 14.2, "Loadable Function Reference". -- https://dev.mysql.com/doc/refman/8.4/en/loadable-function-reference.html
+
+-- Loadable functions contrast with built-in (native) functions,
+-- whic hare implemented as part of the server and a always available;
+-- for a table, see Section 14.1, "Built-In Function and Operator Reference". -- https://dev.mysql.com/doc/refman/8.4/en/built-in-function-reference.html
+
+
+
+
+
+
+
+
