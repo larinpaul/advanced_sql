cebc976a828a086c75aee8af85c1c66893e636a5
larinpaul <www.larinpavel@mail.ru>
2025-02-17 06:17:23 +0300

    -- Why Use SQL Partitioning? -- Imagine trying tofindasepcific book in a library with millions of books and no orgamization system. -- It would be a nightmare! Partitioning is like organizing the library into different sections -- based on genre, author, or publication date. -- This makes it much easier to find what you're looking for.
    -- Similarly, partitioning a large table in the dataset brings numerous advantages:
    
    -- Enhanced Performance and Scalability
    -- * Faster Queries
    -- * Parallel Processing
    -- * Efficient Data Management
    
    -- Streamlined Data Management
    -- * Data Lifecycle Management -- For example, older partitions can be archived or dropped effortlessly.
    -- * Data Locality -- Partitions can reside on different storage devices, even different physical location.
    
    -- Simplified Querying and Analysis
    -- * Partition-based Queries
    -- * Data Summarization

--- 39_sql_partitioning.sql
+++ 39_sql_partitioning.sql
@@ -13,4 +13,28 @@
 -- compared to single partition table.
 
 
+-- Why Use SQL Partitioning?
+-- Imagine trying tofindasepcific book in a library with millions of books and no orgamization system.
+-- It would be a nightmare! Partitioning is like organizing the library into different sections
+-- based on genre, author, or publication date.
+-- This makes it much easier to find what you're looking for.
+
+-- Similarly, partitioning a large table in the dataset brings numerous advantages:
+
+-- Enhanced Performance and Scalability
+-- * Faster Queries
+-- * Parallel Processing
+-- * Efficient Data Management
+
+-- Streamlined Data Management
+-- * Data Lifecycle Management -- For example, older partitions can be archived or dropped effortlessly.
+-- * Data Locality -- Partitions can reside on different storage devices, even different physical location.
+
+-- Simplified Querying and Analysis
+-- * Partition-based Queries
+-- * Data Summarization
+
+
+
+
 
