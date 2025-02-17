1ee2148a4a4f8ecf3c67e477d61b30836ba64e3d
larinpaul <www.larinpavel@mail.ru>
2025-02-17 06:04:21 +0300

    -- SQL Partitioning: A Step-by-Step Guide for Database Optimizaton -- TiDB Team -- https://www.pingcap.com/article/sql-partition-demystified-from-concept-to-implementation/
    -- Remember the days when a gigabyte was considered a lot of data? Yeah, me neither
    -- Today, we're dealing with terabytes and petabytes of data,
    -- and managing these massive datasets efficiently is no small feat.
    -- This is where SQL partitioning comes in, offering a powerful tool
    -- to divide and conquer large tables,
    -- boosting performance, and simplifying data management,
    -- compared to single partition table.

---
+++ 39_sql_partitioning.sql
@@ -0,0 +1,16 @@
+-- 2025 02 17 
+
+-- SQL Partitioning: A Step-by-Step Guide for Database Optimizaton
+-- TiDB Team
+-- https://www.pingcap.com/article/sql-partition-demystified-from-concept-to-implementation/
+
+-- Remember the days when a gigabyte was considered a lot of data? Yeah, me neither
+-- Today, we're dealing with terabytes and petabytes of data,
+-- and managing these massive datasets efficiently is no small feat.
+-- This is where SQL partitioning comes in, offering a powerful tool
+-- to divide and conquer large tables,
+-- boosting performance, and simplifying data management,
+-- compared to single partition table.
+
+
+
