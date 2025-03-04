75bfac12e5bae34eecf2beef0dc56cdfaa89fcf5
larinpaul <www.larinpavel@mail.ru>
2025-03-04 06:14:44 +0300

    -- What is dirty data?
    -- * Incomplete data -- for example, a spreadsheet with missing values that would berelevant for your analysis
    -- * Duplicate data
    -- * Inconsistent or inaccurate data -- data that is outdated or contains structural errors
    -- such as typos, inconsistent capicatiozation, and irregular naming conventions.
    -- For example some are "Pass" or "Fair" and others are "P" or "F".
    
    -- 7 most common types of dirty data and how to clean them
    -- https://pipeline.zoominfo.com/operations/dirty-data-bottom-line
    
    -- ...
    
    -- data experts spend a good 60% of their time on data cleaning.

---
+++ 43_data_cleaning.sql
@@ -0,0 +1,27 @@
+-- 2025 03 04 -- 6:11
+
+-- https://careerfoundry.com/en/tutorials/data-analytics-for-beginners/introduction-to-data-cleaning
+
+-- Tutorial 2: An Introduction to Data Cleaning
+
+
+-- ...
+
+-- What is dirty data?
+
+-- * Incomplete data -- for example, a spreadsheet with missing values that would berelevant for your analysis
+-- * Duplicate data
+-- * Inconsistent or inaccurate data -- data that is outdated or contains structural errors
+-- such as typos, inconsistent capicatiozation, and irregular naming conventions.
+-- For example some are "Pass" or "Fair" and others are "P" or "F".
+
+-- 7 most common types of dirty data and how to clean them
+-- https://pipeline.zoominfo.com/operations/dirty-data-bottom-line
+
+-- ...
+
+-- data experts spend a good 60% of their time on data cleaning.
+
+
+
+
